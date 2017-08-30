2f2e14630d48c61e5306f999148f84826ebc0e66
Alexander Siniouguine <sasha@virtoway.com>
2017-08-29 22:35:41 -0700

    add unauthorized test

--- VirtoCommerce.Marketo.Test/MarketoScenarios.cs
+++ VirtoCommerce.Marketo.Test/MarketoScenarios.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Authentication;
 using VirtoCommerce.Marketo.Data.Models;
 using VirtoCommerce.Marketo.Data.Services;
 using Xunit;
@@ -29,6 +30,31 @@ namespace VirtoCommerce.Marketo.Test
             var result = service.CreateOrUpdateLeads(request).Result;
 
             Assert.True(result.success);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Can_create_leads_unauthorized()
+        {
+            var serviceName = Environment.GetEnvironmentVariable("MarketoUrl");
+            var connection = new MarketoConnectionInfo()
+            {
+                RestApiUrl = Environment.GetEnvironmentVariable("MarketoUrl"),
+                ClientId = "fake",
+                ClientSecret = "fakke"
+            };
+
+            var service = new MarketoService(connection);
+
+            var request = new LeadsRequest
+            {
+                lookupField = "email",
+                input = new[] { new Lead { postalCode = "90069", email = "test@user.com", firstName = "John", lastName = "Doe", leadScore = 10, address = "sample address", city = "los angeles", country = "United States", phone = "234234234", state = "CA" } }
+            };
+
+            var exception = Record.Exception(() => service.CreateOrUpdateLeads(request).Result);
+
+            Assert.IsType(typeof(AuthenticationException), exception.InnerException.InnerException);
         }
     }
 }
